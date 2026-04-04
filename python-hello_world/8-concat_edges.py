84bf31a6904669fbb2565028b7016fa35f09da19
mirresul <13771@holbertonstudents.com>
2026-04-04 17:03:48 +0000

    task_88

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[67] + str[106:110] + str[110] + str[:6], end="")
+print(str[39:67] + str[67:68] + str[106:110] + str[110:111] + str[:6], end="")
 str = str
 str = str
