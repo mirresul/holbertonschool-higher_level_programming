fea382f9a1a7f2ceb1d7799aa1a4300a33522ce5
mirresul <13771@holbertonstudents.com>
2026-04-11 18:16:18 +0000

    squ

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize square with size."""
+        self.__size = size
