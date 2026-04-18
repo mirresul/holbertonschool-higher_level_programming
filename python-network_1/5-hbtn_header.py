89a35cc383e27f333c7b81f94d4fea30ef4ecb78
mirresul <13771@holbertonstudents.com>
2026-04-18 10:57:27 +0000

    task 5 request id header

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Gets X-Request-Id header from response using requests"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    request_id = response.headers.get("X-Request-Id")
+    print(request_id)
