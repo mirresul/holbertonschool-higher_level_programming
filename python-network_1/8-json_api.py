f9422eaf133dfbd18a3bad0c0f489dac0ca690df
mirresul <13771@holbertonstudents.com>
2026-04-18 11:00:47 +0000

    task 8 search api json handling

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Search API script using POST request and JSON handling"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    data = {"q": q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_data = response.json()
+
+        if not json_data:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+
+    except ValueError:
+        print("Not a valid JSON")
